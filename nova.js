dc5851b74b2cda10824fac2a2789d438bf888b1b
akimanagabriel <gabsonbill@yahoo.com>
2022-10-29 01:19:25 +0200

    changes to nova app

--- nova.js
+++ nova.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+
+const users = [{
+    id: 1,
+    name: 'gaby'
+},]
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello heruku app</h1>')
+    res.json(users)
 })
 
 app.listen(port, () => console.log('listening on port: ' + port))
