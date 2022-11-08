8a11d2cfc72563e55e2ee6c6f8595710bd579d73
akimanagabriel <gabsonbill@yahoo.com>
2022-11-08 09:37:49 +0200

    add test

---
+++ configurations/config.js
@@ -0,0 +1,6 @@
+
+const database = {
+    url: 'mongodb+srv://leirbag:123@cluster0.j4g3k.mongodb.net/test'
+}
+
+module.exports = { database }
--- nova.js
+++ nova.js
@@ -1,14 +1,43 @@
+var mongoose = require('mongoose');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const { database } = require('./configurations/config')
+const Test = require('./Models/Test.modal')
 
-const users = [{
-    id: 1,
-    name: 'gaby'
-},]
+
+//Set up default mongoose connection
+mongoose.connect(database.url);
+
+//Get the default connection
+// var db = mongoose.connection;
+
+app.get('/create-test', (req, res) => {
+    var test
+    if (req.query.name) {
+        test = Test({ name: req.query.name })
+    } else {
+        test = Test({ name: 'NodeJs test' })
+    }
+
+    test.save((err) => {
+        if (err) res.json(err)
+        res.json({
+            message: 'test created successfully',
+            data: test
+        })
+    })
+})
 
 app.get('/', (req, res) => {
-    res.json(users)
+    Test.find()
+        .exec((err, tests) => {
+            if (err) res.json({
+                message: 'error occured while finding test',
+                details: err
+            })
+            res.json(tests)
+        })
 })
 
 app.listen(port, () => console.log('listening on port: ' + port))
