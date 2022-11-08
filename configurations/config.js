3f5ad6a5a4d17476e9d4ef41eee55d8f6f21f769
akimanagabriel <gabsonbill@yahoo.com>
2022-11-08 09:58:57 +0200

    add test

--- configurations/config.js
+++ configurations/config.js
@@ -1,6 +1,6 @@
 
 const database = {
-    url: 'mongodb+srv://leirbag:123@cluster0.j4g3k.mongodb.net/test'
+    url: 'mongodb+srv://leirbag:123@cluster0.j4g3k.mongodb.net/?retryWrites=true&w=majority'
 }
 
 module.exports = { database }
